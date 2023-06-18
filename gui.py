ab68132170ac6c43df58878ddd23b92759b30817
ABOTHU SRESHTA BHAVITA <101741015+sreshtabhavita@users.noreply.github.com>
2023-06-19 03:00:45 +0530

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,88 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+import os
+
+from sklearn import metrics
+
+from tensorflow.keras.models import model_from_json
+from PIL import Image, ImageTk
+import numpy as np
+import cv2
+
+# download haarcascade_frontalface_default from here "https://github.com/opencv/opencv/tree/master/data/haarcascades"
+
+def EyeDetectionModel(json_file, weights_file):
+    with open(json_file,"r") as file:
+        loaded_model_json = file.read()
+        model = model_from_json(loaded_model_json)
+
+    model.load_weights(weights_file)
+    model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+    return model
+
+top = tk.Tk()
+top.geometry('800x600')
+top.title('Eye open or close detector')
+top.configure(background='#CDCDCD')
+
+label1 = Label(top, background='#CDCDCD', font=('arial',15,'bold'))
+sign_image = Label(top)
+
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+lefteye_cascade = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
+
+model = EyeDetectionModel("model_a1.json","model_weights1.h5")
+
+
+EYE_LIST = ["Close", "Open"]
+
+def DetectEyes(file_path):
+    global label_packed
+
+    image = cv2.imread(file_path)
+    gray_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    eyes = eye_cascade.detectMultiScale(gray_image, 1.3, 5)
+    try:
+        for (x,y,w,h) in eyes:
+            fc = gray_image[y:y+h,x:x+w]
+            roi = cv2.resize(fc,(48,48))
+            pred = EYE_LIST[np.argmax(model.predict(roi[np.newaxis,:,:,np.newaxis]))]
+        print("Prediction is " + pred)
+        label1.configure(foreground="#011638", text = pred)
+    except:
+        label1.configure(foreground='#011638', text="Unable to detect")
+
+
+def show_Detect_button(file_path):
+    detect_b = Button(top,text="Detect Open/Close", command=lambda: DetectEyes(file_path),padx=10,pady=5)
+    detect_b.configure(background="#364146", foreground='white', font=('arial',10,'bold'))
+    detect_b.place(relx=0.79, rely=0.46)
+
+
+def upload_image():
+    try:
+        file_path = filedialog.askopenfilename()
+        uploaded = Image.open(file_path)
+        uploaded.thumbnail(((top.winfo_width()/2.25),(top.winfo_height()/2.25)))
+        im = ImageTk.PhotoImage(uploaded)
+
+        sign_image.configure(image = im)
+        sign_image.image = im
+        label1.configure(text ='')
+        show_Detect_button(file_path)
+    except:
+        pass
+
+
+upload = Button(top,text = "Upload Image", command = upload_image, padx=10, pady=5)
+upload.configure(background="#364156", foreground = 'white', font=('arial',20,'bold'))
+upload.pack(side='bottom',pady=50)
+sign_image.pack(side='bottom', expand='True')
+label1.pack(side='bottom',expand='True')
+heading=Label(top,text='Eyes Open or Close Detection', pady=20, font=('arial',25,'bold'))
+heading.configure(background='#CDCDCD', foreground="#364156")
+heading.pack()
+top.mainloop()
+ 
